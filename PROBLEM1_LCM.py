6f69ab4aa6f7e0cc1a7dccb10b65f3740abea551
SP3CTR021 <g.belena.555755@umindanao.edu.ph>
2025-09-18 11:56:17 +0800

    Add files via upload

---
+++ PROBLEM1_LCM.py
@@ -0,0 +1,11 @@
+#Problem 1
+def gcm (a,b):
+    return a*b
+
+x = (int("Please enter  a value for x: "))
+
+y =(int("Please enter a value for y: "))
+
+int lsm = gcm(x,y)
+
+print = (f"The lsm of {x} and {y} is ")
