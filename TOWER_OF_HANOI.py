09cf9cf0d4ea454a7aa34d949d126159ddfd2ea5
SP3CTR021 <g.belena.555755@umindanao.edu.ph>
2025-09-18 12:47:26 +0800

    Delete TOWER_OF_HANOI.py

--- TOWER_OF_HANOI.py
+++
@@ -1,9 +0,0 @@
-#Problem 2
-
-def tower_of_hanoi(n, source, target, auxilliary) :
-
-    if (n == 1):
-        print (f"move disk 1 from {source} to {target}")
-        return
-        
-tower_of_hanoi(n-1, source, auxilliary,target,)
